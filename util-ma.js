0f97ba0375a2457ed926c1efc5da59327ec78f07
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2019-10-26 02:19:17 +0330

    formatting

--- util-ma.js
+++ util-ma.js
@@ -9,32 +9,34 @@
   }
 })((function () {
   'use strict';
-  // arrays
-  function moveArrItem(a, f, t) { // array, from, to
-    a.splice( t, 0, a.splice(f, 1)[0] );
+  function isObj(v) {
+    return (
+      v &&
+      typeof v === 'object' &&
+      typeof v !== null &&
+      Object.prototype.toString.call(v) === '[object Object]'
+    ) ? true : false;
   }
-  // strings
-  function isEmptyStr(v) {
-    return typeof v === 'string'  &&  v.length === 0;
+	function isNull(v) {
+    return typeof v === 'object' && v === null;
   }
-  function substrBeforeLast(c, s) {
-    return s.substr( 0, s.lastIndexOf(c) );
-  }
-  function substrAfterLast(c, s) {
-    return s.substring(s.lastIndexOf(c) + 1);
-  }
-  function substrBeforeFirst(c, s) {
-    return s.substr( 0, s.indexOf(c) );
-  }
-  function substrAfterFirst(c, s) {
-    return s.substring(s.indexOf(c) + 1);
-  }
-  // numbers
-  function isInt(n) {
+	function isInt(n) {
     return n % 1 === 0;
   }
   function isNumOdd(n) {
 		return n % 2;
+  }
+  function isEmptyObj(o) {
+    if ( isObj(o) ) {
+      return Object.getOwnPropertyNames(o).length === 0;
+    }
+    return false;
+  }
+  function isEmptyStr(v) {
+    return typeof v === 'string'  &&  v.length === 0;
+  }
+	function moveArrItem(a, f, t) { // array, from, to
+    a.splice( t, 0, a.splice(f, 1)[0] );
   }
   function negateNum(n) {
     return Math.abs(n) * -1;
@@ -62,26 +64,20 @@
   function toDecimalPlace(n, p) {
     return parseFloat( n.toFixed(p) );
   }
-  // misc
-  function isNull(v) {
-    return typeof v === 'object' && v === null;
+  function substrBeforeLast(c, s) {
+    return s.substr( 0, s.lastIndexOf(c) );
   }
-  // objects
-  function isObj(v) {
-    return (
-      v &&
-      typeof v === 'object' &&
-      typeof v !== null &&
-      Object.prototype.toString.call(v) === '[object Object]'
-    ) ? true : false;
+  function substrAfterLast(c, s) {
+    return s.substring(s.lastIndexOf(c) + 1);
   }
-  function isEmptyObj(o) {
-    if ( isObj(o) ) {
-      return Object.getOwnPropertyNames(o).length === 0;
-    }
-    return false;
+  function substrBeforeFirst(c, s) {
+    return s.substr( 0, s.indexOf(c) );
   }
-  function extend() {
+  function substrAfterFirst(c, s) {
+    return s.substring(s.indexOf(c) + 1);
+  }
+	
+	function extend() {
     var args = getArgs(arguments),
       len = args.length,
       arr = [],
@@ -131,8 +127,6 @@
 	
   return {
     isObj,
-    isArr,
-    getArgs,
     moveArrItem,
     isInt,
     negateNum,
@@ -143,15 +137,8 @@
     randFloat,
     toDecimalPlace,
     isEmptyObj,
-    isFn,
-    isStr,
-    isNum,
-    isNAN,
-    isBool,
-    isUndef,
     isNull,
     isEmptyStr,
-    objLength,
     substrBeforeLast,
     substrAfterLast,
     substrBeforeFirst,
